1f24ddbf1dedfca163bb75137937a93c235fa2f5
udaykiran <udaykiranraga@gmail.com>
2024-08-27 14:34:56 +0530

    Updated index.css and Contact.jsx with new styles and components

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -21,6 +21,30 @@ const Contact = () => {
             className='form_input mt-1'
             />
           </div>
+          <div>
+            <label htmlFor="subject" className='form_label'>
+              Subject
+            </label>
+            <input 
+            type="text" 
+            id='subject'
+            placeholder='let us know how we can help you'
+            className='form_input mt-1'
+            />
+          </div>
+          <div className='sm:col-span-2'>
+            <label htmlFor="message" className='form_label'>
+              Your Message
+            </label>
+            <textarea 
+            rows='6'
+            type="text" 
+            id='message'
+            placeholder='leave a comment....'
+            className='form_input mt-1'
+            />
+          </div>
+          <button type='submit' className='btn rounded sm:w-fit'>Submit</button>
         </form>
       </div>
     </section>
